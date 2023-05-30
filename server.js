43e42820a68b4063f50f6d05991e94aef4ecd831
unknown <ecblickenderfer@gmail.com>
2023-05-30 18:37:32 -0400

    rearranged file paths, removed ejs

--- server.js
+++ server.js
@@ -22,11 +22,68 @@ app.get("/", (req, res)=>{
 
 app.get("/notes", (req, res)=>{
     let dbNotes = JSON.parse(fs.readFileSync("./db/db.json"))
-    res.render("notes", {
-        noteList: dbNotes
+    let listString = ""
+    dbNotes.forEach(oneNote => {
+        listString+=
+        `<li data-note="{'text':${oneNote.text}, 'title': ${oneNote.title}, 'id': ${oneNote.id}}" style="display:flex; justify-content: space-between;"><span onclick="handleNoteView()">${oneNote.title}</span><button class="deleteBtn" onclick="deleteNote('${oneNote.id}')">Delete</button></li>`
     })
-})
+    let htmlString = `
+    <!DOCTYPE html>
+    <html lang="en-US">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Note Taker</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+          crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="/assets/css/styles.css" />
+      </head>
+    
+      <body>
+        <nav class="navbar bg-info">
+          <a class="navbar-brand text-light p-3" href="/">Note Taker </a>
+          <div class="icons">
+            <i class="fas fa-save text-light save-note" onclick="saveNote()"></i>
+            <i class="fas fa-plus text-light new-note"></i>
+          </div>
+        </nav>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-4 list-container">
+              <div class="card">
+                <ul class="list-group">
+                  ${listString}
+                </ul>
+              </div>
+            </div>
+            <div class="col-8">
+              <input
+                onkeyup="checkText()"
+                id="titleField"
+                name="noteTitle"
+                class="note-title"
+                placeholder="Note Title"
+                maxlength="28"
+                type="text"
+               
+              />
+              <textarea onkeyup="checkText()" id="textField" name="noteText" class="note-textarea" placeholder="Note Text"></textarea>
+            </div>
+          </div>
+        </div>
+        <script src="./assets/js/index.js"></script>
+      </body>
+    </html>`
+    
 
+res.send(htmlString)
+})
 app.get("/notes/:id", (req, res) =>{
     let dbNotes = JSON.parse(fs.readFileSync("./db/db.json"))
     for(let i=0; i<dbNotes.length; i++){
@@ -45,7 +102,7 @@ app.post("/notes", (req, res) =>{
     let newNote = req.body
     newNote.id = id.v1()
     dbNotes.push(newNote)
-    fs.writeFileSync("db/db.json", json.stringify(dbNotes))
+    fs.writeFileSync("db/db.json", JSON.stringify(dbNotes))
     let dbFile = JSON.parse(fs.readFileSync("./db/db.json"))
-    res.render("notes", {noteList:dbFile})
+    // res.render("notes", {noteList:dbFile})
 })
